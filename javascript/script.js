98e7a5efc30d8c273b8d41535d0c148c50569513
Your Name <you@example.com>
2024-08-05 06:01:30 -0700

    java script

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+// console.log("hello world");
+// console.log("this is our first msg");
+// let num =20; //int data type
+// let num2 = 2.40 //float data type
+// let message = "wordpress" //sting data type
+// arithmetic operations
+// console.log(2+2)
+// console.log("2+2")
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2-2)
+// let num = 10;
+// let num2 = 20;
+// console.log(num+num2)
+// let num7 = 10;
+// let num3 = 20;
+// console.log(num3-num7)
+// aasignment operater
+// num1 =5;
+// num1 += 5;
+// console.log(num1)
+// let num=1;
+// let num2=2;
+// console.log("num1-num2");
