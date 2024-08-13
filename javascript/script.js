0e937f4153c07b80b3318a5f8d700d9a537ca8e5
Your Name <you@example.com>
2024-08-13 05:01:00 -0700

    assignment2

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,77 @@
-// console.log("hello world");
-// console.log("this is our first msg");
-// let num =20; //int data type
-// let num2 = 2.40 //float data type
-// let message = "wordpress" //sting data type
-// arithmetic operations
-// console.log(2+2)
-// console.log("2+2")
-// console.log(2*2)
-// console.log(2/2)
-// console.log(2-2)
-// let num = 10;
-// let num2 = 20;
-// console.log(num+num2)
-// let num7 = 10;
-// let num3 = 20;
-// console.log(num3-num7)
-// aasignment operater
-// num1 =5;
-// num1 += 5;
-// console.log(num1)
-// let num=1;
-// let num2=2;
-// console.log("num1-num2");
+// let num1=30;
+// let num2=20;
+// sum=num1+num2
+// difference=num1-num2
+// product=num1*num2
+// quotient=num1/num2
+// console.log(sum)
+// console.log(difference)
+// console.log(product)
+// console.log(quotient)
+// comparision operators
+// let num=40
+// if (num>=40){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+// if (num>=40){console.log("true")}
+// if (num==40){console.log("correct")}
+// if (num!=40){console.log("false")}
+// if (num<40){console.log("wrong")}
+// if (num>40){console.log("wrong")}
+// if (num>=40){console.log("false")}
+// And operator
+// let num=10
+// if(num<15 && num>5){console.log("num is btw 6 and 15")}
+// Or operator
+// let num1 = 20;
+// let num2 = 30;
+// if(num1==20 || num2==20){console.log("num1 and num2 is equal")}
+// // NOT operator
+// let num=2;
+// let num1=3;
+// if(!(num==num1)){console.log("num and num1 are equal")}
+// else if
+// let day="monday";
+// if(day=="monday"){console.log("correct day")}
+// else if(day=="tuesday"){console.log("wrong day")}
+// else if(day=="wednesday"){console.log("wrong day")}
+// else if(day=="thursday"){console.log("wrong day")}
+// else if(day=="friday"){console.log("false")}
+// else if(day=="saturday"){console.log("wrong day")}
+// else{ console.log("wrong day")
+// }
+// switch statment
+// let day="friday"
+// switch(day){
+//   case "monday":
+//     console.log("correct  day")
+//     break;
+//   case "tuesday":
+//     console.log("wrong day")
+//     break;
+//     case "wednesday":
+//     console.log("wrong day")
+//     break;
+//     case "thursday":
+//     console.log("wrong day")
+//     break;
+//     case "friday":
+//     console.log("wrong day")
+//     break;
+//     case "saturday":
+//     console.log("wrong day")
+//     break;
+//     case "sunday":
+//     console.log("wrong day")
+//     break;
+// }
+// looping structures
+// for ( i = 0; i < 10; i++){
+// console.log("hello world")}
+// +i+1 for separate line
+// table of 2
+// for(i=1;i<=10;i++){
+//     console.log( 2*i);
+ 
